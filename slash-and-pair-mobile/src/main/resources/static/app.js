255e339f407e1e468d57468dd3bf18898761e387
vfernaco <v.fern@hotmail.es>
2017-06-12 20:50:20 +0200

    No sleep js

--- slash-and-pair-mobile/src/main/resources/static/app.js
+++ slash-and-pair-mobile/src/main/resources/static/app.js
@@ -65,7 +65,8 @@ $(document).ready(function() {
     connect();
     console.log("connected socket");
     sendSomeDataMobile();
-   
+    var noSleep = new NoSleep();
+    noSleep.enable(); // keep the screen on!
     
 });
     
